4c50c7de141cfc380a656b8d69fafef1d9e11b43
Ellenprah <101070545+Ellenprah@users.noreply.github.com>
2023-04-24 20:19:53 +0000

    Delete domain directory

--- domain/repositories/daily_stats_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:diary/core/errors/failures.dart';
-import 'package:diary/domain/entities/daily_stats.dart';
-import 'package:diary/domain/entities/daily_stats_response.dart';
-
-abstract class DailyStatsRepository {
-  Future<Either<Failure, DailyStatsResponse>> sendDailyStats(
-      DailyStats dailyStats);
-}
